38a7690e152528dc455374b7a80a54b58c76f259
thirtydracs <101961664+thirtydracs@users.noreply.github.com>
2026-04-06 19:44:39 +1000

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,46 @@
-
+const inductionData = [
+  {
+    "[Full Legal Name] First Name": "Anthony",
+    "[Full Legal Name] Last Name": "Buchhorn",
+    "Phone Number": "0466 592 990",
+    "Email": "anthony@powersolutions.net.au",
+    "Company Name": "Power Solutions",
+    "[Emergency Contact] First Name": "Paul",
+    "[Emergency Contact] Last Name": "Buchhorn",
+    "Emergency Phone Number": "0466 592 989",
+    "Submission Date": "04/02/2026 09:34 AM"
+  },
+  {
+    "[Full Legal Name] First Name": "Quinn",
+    "[Full Legal Name] Last Name": "Gscheidle",
+    "Phone Number": "0407 759 262",
+    "Email": "shides@outlook.com",
+    "Company Name": "Plumbing Gas Earthmoving",
+    "[Emergency Contact] First Name": "Grace",
+    "[Emergency Contact] Last Name": "Gibb",
+    "Emergency Phone Number": "0401 275 382",
+    "Submission Date": "27/02/2026, 12:26:44 pm"
+  },
+  {
+    "[Full Legal Name] First Name": "Luke",
+    "[Full Legal Name] Last Name": "Beutel",
+    "Phone Number": "0418 667 833",
+    "Email": "lukebeutel@icloud.com",
+    "Company Name": "Plumbing Gas Earthmoving",
+    "[Emergency Contact] First Name": "Dee-Anne",
+    "[Emergency Contact] Last Name": "Beutel",
+    "Emergency Phone Number": "0477 630 004",
+    "Submission Date": "27/02/2026, 12:29:06 pm"
+  },
+  {
+    "[Full Legal Name] First Name": "Steven",
+    "[Full Legal Name] Last Name": "Kurittu",
+    "Phone Number": "0412 433 119",
+    "Email": "skurittu.mck@gmail.com",
+    "Company Name": "McKinnon Earthmoving",
+    "[Emergency Contact] First Name": "Jenna",
+    "[Emergency Contact] Last Name": "Kurittu",
+    "Emergency Phone Number": "0429 652 323",
+    "Submission Date": "03/03/2026 07:58 AM"
+  }
+];
