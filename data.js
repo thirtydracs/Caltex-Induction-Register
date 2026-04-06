f90d0d20e4896fa548aa101de472d53458779562
thirtydracs <101961664+thirtydracs@users.noreply.github.com>
2026-04-06 19:55:48 +1000

    Update data.js

--- data.js
+++ data.js
@@ -42,5 +42,126 @@ const inductionData = [
     "[Emergency Contact] Last Name": "Kurittu",
     "Emergency Phone Number": "0429 652 323",
     "Submission Date": "03/03/2026 07:58 AM"
+  },
+  {
+    "[Full Legal Name] First Name": "Samuel",
+    "[Full Legal Name] Last Name": "Hollander",
+    "Phone Number": "0479 037 967",
+    "Email": "sam@powersolutions.net.au",
+    "Company Name": "Power Solutions",
+    "[Emergency Contact] First Name": "Grace",
+    "[Emergency Contact] Last Name": "Hollander",
+    "Emergency Phone Number": "0411 386 126",
+    "Submission Date": "03/03/2026 08:16 AM"
+  },
+  {
+    "[Full Legal Name] First Name": "Lisa",
+    "[Full Legal Name] Last Name": "Newman",
+    "Phone Number": "0408 747 730",
+    "Email": "lisa@arcanacapital.com",
+    "Company Name": "Arcana Capital",
+    "[Emergency Contact] First Name": "Campbell",
+    "[Emergency Contact] Last Name": "Newman",
+    "Emergency Phone Number": "0400 779 900",
+    "Submission Date": "03/03/2026 11:52 AM"
+  },
+  {
+    "[Full Legal Name] First Name": "Mitchell",
+    "[Full Legal Name] Last Name": "Tabikh",
+    "Phone Number": "0483 816 461",
+    "Email": "mitchtabikh@gmail.con",
+    "Company Name": "Pge",
+    "[Emergency Contact] First Name": "Katrina",
+    "[Emergency Contact] Last Name": "Tabikh",
+    "Emergency Phone Number": "4409276495",
+    "Submission Date": "16/03/2026, 9:05:25 am"
+  },
+  {
+    "[Full Legal Name] First Name": "Samuel",
+    "[Full Legal Name] Last Name": "Collins",
+    "Phone Number": "0408 593 391",
+    "Email": "samcollinsplumbing@hotmail.com",
+    "Company Name": "Plumbing Gas Earthmoving pty Ltd",
+    "[Emergency Contact] First Name": "Vicki",
+    "[Emergency Contact] Last Name": "Collins",
+    "Emergency Phone Number": "0402 231 351",
+    "Submission Date": "16/03/2026, 11:17:44 am"
+  },
+  {
+    "[Full Legal Name] First Name": "Duane",
+    "[Full Legal Name] Last Name": "Ashlin",
+    "Phone Number": "0437 076 605",
+    "Email": "duanesconcrete@gmail.com",
+    "Company Name": "Guillotine concrete",
+    "[Emergency Contact] First Name": "Ty",
+    "[Emergency Contact] Last Name": "Gilhurst",
+    "Emergency Phone Number": "0449 139 560",
+    "Submission Date": "31/03/2026, 4:02:24 pm"
+  },
+  {
+    "[Full Legal Name] First Name": "Justin",
+    "[Full Legal Name] Last Name": "Dillon",
+    "Phone Number": "0421 329 435",
+    "Email": "justindillon86@hotmail.com",
+    "Company Name": "Mobile Conveying Services",
+    "[Emergency Contact] First Name": "Amelia",
+    "[Emergency Contact] Last Name": "Coulton",
+    "Emergency Phone Number": "0402 339 591",
+    "Submission Date": "03/05/2026 08:19 AM"
+  },
+  {
+    "[Full Legal Name] First Name": "Jaxson",
+    "[Full Legal Name] Last Name": "Flay",
+    "Phone Number": "0431 886 942",
+    "Email": "jaxsonflay@gmail.com",
+    "Company Name": "Mobile conveying services",
+    "[Emergency Contact] First Name": "Gabbi",
+    "[Emergency Contact] Last Name": "Flay",
+    "Emergency Phone Number": "0426 197 414",
+    "Submission Date": "03/05/2026 08:21 AM"
+  },
+  {
+    "[Full Legal Name] First Name": "Darrel",
+    "[Full Legal Name] Last Name": "Mcleary",
+    "Phone Number": "0419 664 740",
+    "Email": "darrelmcleary@gmail.com",
+    "Company Name": "Borger cranes",
+    "[Emergency Contact] First Name": "Tanya",
+    "[Emergency Contact] Last Name": "Mcleary",
+    "Emergency Phone Number": "0408 664 740",
+    "Submission Date": "03/05/2026 09:17 AM"
+  },
+  {
+    "[Full Legal Name] First Name": "Mark",
+    "[Full Legal Name] Last Name": "Seagrott",
+    "Phone Number": "0457 837 783",
+    "Email": "m_seagrott@bigpond.com",
+    "Company Name": "Borger cranes",
+    "[Emergency Contact] First Name": "Vicki",
+    "[Emergency Contact] Last Name": "Fletcher",
+    "Emergency Phone Number": "0433 147 818",
+    "Submission Date": "03/05/2026 09:19 AM"
+  },
+  {
+    "[Full Legal Name] First Name": "Graham",
+    "[Full Legal Name] Last Name": "Lawson",
+    "Phone Number": "0418 877 944",
+    "Email": "graham@doswater.com.au",
+    "Company Name": "4D Installation",
+    "[Emergency Contact] First Name": "Graham",
+    "[Emergency Contact] Last Name": "Lawson",
+    "Emergency Phone Number": "0418 877 944",
+    "Submission Date": "03/05/2026 11:46 AM"
+  },
+  {
+    "[Full Legal Name] First Name": "David",
+    "[Full Legal Name] Last Name": "Batchelor",
+    "Phone Number": "0423 735 750",
+    "Email": "dbatchelor@4dinstallations.com.au",
+    "Company Name": "4D",
+    "[Emergency Contact] First Name": "Karen",
+    "[Emergency Contact] Last Name": "Batchelor",
+    "Emergency Phone Number": "0414 513 582",
+    "Submission Date": "03/05/2026 11:53 AM"
   }
 ];
